fe32f12d48b980dfa9f69671e824889bf89e82f8
Jim Meløysund <jim.meloysund@am-d006mq627j.felles.ds.nrk.no>
2024-04-18 13:25:39 +0200

    fixes

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,51 @@
   import svelteLogo from "./assets/svelte.svg";
   import viteLogo from "/vite.svg";
   import Counter from "./lib/Counter.svelte";
+
+  import { onMount } from "svelte";
+  let timerStarted = false;
+  let startTime;
+  let elapsedTime = 0;
+  let countdown = 30;
+  const timeleft = 30;
+  let hasstarted = false;
+
+  const handleStart = () => {
+    if (!hasstarted) {
+      hasstarted = true;
+      startTimer();
+    }
+  };
+
+  const startTimer = () => {
+    if (!timerStarted) {
+      startTime = Date.now();
+      timerStarted = true;
+      const timerInterval = setInterval(() => {
+        elapsedTime = Date.now() - startTime;
+        countdown = Math.max(0, timeleft - Math.floor(elapsedTime / 1000));
+        if (countdown === 0) {
+          stopTimer();
+          gameOver = true;
+        }
+      }, 10);
+      return timerInterval;
+    }
+  };
+  const stopTimer = () => {
+    clearInterval(timerInterval);
+    timerStarted = false;
+  };
+  let timerInterval;
+  // onMount(() => {
+  //   timerInterval = startTimer();
+  // });
 </script>
 
 <main>
-  <div class="card">
+  <h2>Trykk så mange gang du klarer på {timeleft} Sekunder!</h2>
+  <h2>{countdown}</h2>
+  <div on:click={handleStart} class="card">
     <Counter />
   </div>
 </main>
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,28 @@
 <script>
-  let count = 0
+  let count = 0;
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
 </script>
 
 <button on:click={increment}>
-  count is {count}
+  {count}
 </button>
+
+<style>
+  button {
+    background-color: #f9396b; /* Green */
+    border: none;
+    color: white;
+    padding: 30px 64px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 32px;
+    transition: transform 50ms ease-out;
+  }
+
+  button:active {
+    transform: scale(1.4);
+  }
+</style>
